3ebbc38771fcd7724ddb3d4674d1caea6ad46961
SRIDHARAN <81850780+Sridharanraja@users.noreply.github.com>
2025-03-11 08:55:38 +0530

    pysqlite3-binary

--- app.py
+++ app.py
@@ -1,5 +1,9 @@
 API='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'
 
+__import__('pysqlite3')
+import sys
+sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
+
 import streamlit as st
 import sqlite3
 import json
